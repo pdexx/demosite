bd0c0632b0d5c2f84551b6f11852a693a73f80de
pdex <isupd2002@yahoo.com.tw>
2017-08-22 12:06:09 +0800

    mode develop

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 window.onload = function () {
-
+  var mode=0;
   var el = document.querySelectorAll('.box');
-  var cardNumber = el.length;//抓取卡片的張數
+  var cardNumber;//= el.length;//抓取卡片的張數
+  if(mode===0){cardNumber=3;}//else if(){}else{}
+  console.log(cardNumber);
   var color = [];//同等卡片張數的顏色數量
   var ansCard = pickcard();//選到的答案
-  console.log(ansCard);
   var el2 = document.querySelector('h1');
   var el3 = document.querySelector('p');
   var el4 = document.querySelector('body');
-  var el5 = document.querySelector('a');
+  var el5 = document.querySelector('#btnAgain');
+  var el6 = document.querySelector('#mode');
   el5.addEventListener('click', fadeOut);
+  el5.addEventListener('click', function () { el6.style.opacity = 0; });
   el5.addEventListener('click', function () { el4.style.backgroundColor = 'black'; });
   el3.textContent = 'WHAT\'S THE COLOR?';
   cardColorGenerate();//先產生顏色
-
   for (var i = 0; i < cardNumber; i++) {//給每張卡顏色與遊戲功能
     el[i].style.backgroundColor = color[i];
     el[i].addEventListener('click', fadeOut);
@@ -40,6 +42,7 @@ window.onload = function () {
         el[i].style.backgroundColor = "#fff";
         el[i].style.opacity = 1;
         el5.style.opacity = 1;
+        el6.style.opacity = 1;
       }
     } else {
       el3.textContent = 'TRY ANOTHER COLOR';
